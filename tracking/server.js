cdb851136237775628ea607f0d72d50df278c09c
IsaacGu <st.isaac.gu@me.com>
2018-09-25 09:33:40 +0800

    some changes

--- tracking/server.js
+++ tracking/server.js
@@ -9,7 +9,6 @@ const __gif = fs.readFileSync("./__utm.gif");
 
 app.get("/__utm.gif", function (req, res) {
 	console.info(req.query);
-
 	res.writeHead(200, {'content-type': 'image/gif'});
 	res.end(__gif);
 });
